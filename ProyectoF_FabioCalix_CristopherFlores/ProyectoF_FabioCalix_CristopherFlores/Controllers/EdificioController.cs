448ddb568b8f66667c08f50e45dac9a996084b3a
AlexanderComayagua <78757169+AlexanderComayagua@users.noreply.github.com>
2025-04-06 15:30:43 -0600

    Implementación de Controladores Apartamento/Edificio + Vistas Básicas -Cristopher Flores

---
+++ ProyectoF_FabioCalix_CristopherFlores/ProyectoF_FabioCalix_CristopherFlores/Controllers/EdificioController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web.Http;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Newtonsoft.Json;
+using ProyectoF_FabioCalix_CristopherFlores.Models;
+
+namespace ProyectoF_FabioCalix_CristopherFlores.Controllers
+{
+    public class EdificioController : ApiController
+    {
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get(DataSourceLoadOptions loadOptions)
+        {
+            var apiUrl = "https://localhost:44331/api/Edificio";
+
+            var respuestaJson = await GetAsync(apiUrl);
+            List<Edificio> listEdificio = JsonConvert.DeserializeObject<List<Edificio>>(respuestaJson);
+            return Request.CreateResponse(DataSourceLoader.Load(listEdificio, loadOptions));
+        }
+
+        public static async Task<string> GetAsync(string uri)
+        {
+            try
+            {
+                var handler = new HttpClientHandler();
+                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+                using (var client = new HttpClient(handler))
+                {
+                    var response = await client.GetAsync(uri);
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+            }
+            catch (Exception e)
+            {
+                var m = e.Message;
+                return null;
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post(FormDataCollection form)
+        {
+
+            var values = form.Get("values");
+
+            var httpContent = new StringContent(values, System.Text.Encoding.UTF8, "application/json");
+
+            var url = "https://localhost:44331/api/Edificio";
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (var client = new HttpClient(handler))
+            {
+                var response = await client.PostAsync(url, httpContent);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+
+
+        [HttpDelete]
+        public async Task<HttpResponseMessage> Delete(FormDataCollection form)
+        {
+            var key = Convert.ToInt32(form.Get("key"));
+
+            var apiUrlDelEdf = "https://localhost:44331/api/Edificio/" + key;
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (var client = new HttpClient(handler))
+            {
+                var respuestaArrd = await client.DeleteAsync(apiUrlDelEdf);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+
+        [HttpPut]
+        public async Task<HttpResponseMessage> Put(FormDataCollection form)
+        {
+            var key = Convert.ToInt32(form.Get("key"));
+            var values = form.Get("values");
+
+            var apiUrlGetdf = $"https://localhost:44331/api/Edificio/{key}";
+            var respuestaEdificio = await GetAsync(apiUrlGetdf);
+            Edificio edificio = JsonConvert.DeserializeObject <Edificio>(respuestaEdificio);
+
+            JsonConvert.PopulateObject(values, edificio);
+
+            string jsonString = JsonConvert.SerializeObject(edificio);
+            var httpContent = new StringContent(jsonString, System.Text.Encoding.UTF8, "application/json");
+
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (var client = new HttpClient(handler))
+            {
+                var url = $"https://localhost:44331/api/Edificio/{key}";
+                var response = await client.PutAsync(url, httpContent);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+            }
+
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
