0e4c5bb37919f111e8f1a15e48e2675e78759687
AlexanderComayagua <78757169+AlexanderComayagua@users.noreply.github.com>
2025-04-06 19:03:02 -0600

    Implementación de controladores Contrato/Pago/Factura + Vistas básicas (Factura y Pago aun no funcionales) -Cristopher Flores

---
+++ ProyectoF_FabioCalix_CristopherFlores/ProyectoF_FabioCalix_CristopherFlores/Controllers/ContratoController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web.Http;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Newtonsoft.Json;
+using ProyectoF_FabioCalix_CristopherFlores.Models;
+
+namespace ProyectoF_FabioCalix_CristopherFlores.Controllers
+{
+    public class ContratoController : ApiController
+    {
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get(DataSourceLoadOptions loadOptions)
+        {
+            var apiUrl = "https://localhost:44331/api/Contrato";
+
+            var respuestaJson = await GetAsync(apiUrl);
+            List<Contrato> listContrato = JsonConvert.DeserializeObject<List<Contrato>>(respuestaJson);
+            return Request.CreateResponse(DataSourceLoader.Load(listContrato, loadOptions));
+        }
+
+        public static async Task<string> GetAsync(string uri)
+        {
+            try
+            {
+                var handler = new HttpClientHandler();
+                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+                using (var client = new HttpClient(handler))
+                {
+                    var response = await client.GetAsync(uri);
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+            }
+            catch (Exception e)
+            {
+                var m = e.Message;
+                return null;
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post(FormDataCollection form)
+        {
+
+            var values = form.Get("values");
+
+            var httpContent = new StringContent(values, System.Text.Encoding.UTF8, "application/json");
+
+            var url = "https://localhost:44331/api/Contrato";
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (var client = new HttpClient(handler))
+            {
+                var response = await client.PostAsync(url, httpContent);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+
+
+        [HttpDelete]
+        public async Task<HttpResponseMessage> Delete(FormDataCollection form)
+        {
+            var key = Convert.ToInt32(form.Get("key"));
+
+            var apiUrlDelCtr = "https://localhost:44331/api/Contrato/" + key;
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (var client = new HttpClient(handler))
+            {
+                var respuestaContrato = await client.DeleteAsync(apiUrlDelCtr);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+
+        [HttpPut]
+        public async Task<HttpResponseMessage> Put(FormDataCollection form)
+        {
+            var key = Convert.ToInt32(form.Get("key"));
+            var values = form.Get("values");
+
+            var apiUrlGetCtr = $"https://localhost:44331/api/Contrato/{key}";
+            var respuestaContrato = await GetAsync(apiUrlGetCtr);
+            Contrato contrato = JsonConvert.DeserializeObject<Contrato>(respuestaContrato);
+
+            JsonConvert.PopulateObject(values, contrato);
+
+            string jsonString = JsonConvert.SerializeObject(contrato);
+            var httpContent = new StringContent(jsonString, System.Text.Encoding.UTF8, "application/json");
+
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (var client = new HttpClient(handler))
+            {
+                var url = $"https://localhost:44331/api/Contrato/{key}";
+                var response = await client.PutAsync(url, httpContent);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+            }
+
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
